f211c1346702afd073222702ace5aa0e84abc03a
Aditya Kumar <55887731+arch-raven@users.noreply.github.com>
2024-06-29 17:22:59 +0530

    :tada: Modify main.ts to Rename Embeds Plugin

--- main.ts
+++ main.ts
@@ -1,134 +1,53 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {MarkdownView, Notice, Plugin, TFile } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+export default class EmbedRenamePlugin extends Plugin {
+    async onload() {
+        this.addCommand({
+            id: 'rename-embeds-in-file',
+            name: 'Rename Embeds in Current File',
+            checkCallback: (checking: boolean) => {
+                const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+                if (activeView && activeView.file) {
+                    if (!checking) {
+                        this.renameEmbedsInFile(activeView.file);
+                    }
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+    async renameEmbedsInFile(file: TFile) {
+        const attachment_dir = this.app.vault.getConfig("attachmentFolderPath");
+        console.log(`attachment_dir: `, attachment_dir);
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+        let embeds =  this.app.metadataCache.getFileCache(file)?.embeds;
+        if (!embeds) {
+            console.log(`No embeds found in file: `, file.path);
+            new Notice(`No embeds found in file: ${file.path}`);
+            return;
+        }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+        for (let embed of embeds) {
+            console.log(`embed: `, embed);
+            let fullpath = this.app.vault.getFileByPath(embed.link);
+            if (!fullpath) {
+                console.log(`Relative path Not Found: `, embed.link);
+                console.log(`Trying absolute path: : `, `${attachment_dir}/${embed.link}`);
+                fullpath = this.app.vault.getFileByPath(`${attachment_dir}/${embed.link}`);
+            }
+            
+            if (!fullpath) {
+                console.log(`Absolute path also not found: `, `${attachment_dir}/${embed.link}`);
+                continue;
+            }
 
-	async onload() {
-		await this.loadSettings();
+            let dest_fullpath = await this.app.fileManager.getAvailablePathForAttachment(`${file.basename}.${fullpath.extension}`, file.path);
+            await this.app.fileManager.renameFile(fullpath, dest_fullpath);
+            console.log(`Renamed ${fullpath.path} to ${dest_fullpath}`);
+            new Notice(`Renamed ${fullpath.path} to ${dest_fullpath}`);
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+        }
+    }
 }
